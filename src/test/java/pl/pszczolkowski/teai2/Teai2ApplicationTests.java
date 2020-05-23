dc2150d39a62afea1b022d305b7c0b7225e930d8
apispl <krlpl17@gmail.com>
2020-05-23 19:20:56 +0200

    make it better

---
+++ src/test/java/pl/pszczolkowski/teai2/Teai2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pszczolkowski.teai2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Teai2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
